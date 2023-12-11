8d51d57474124eb40f0e480e45770175d88c22b0
Guilherme <guilhermesdias.git@gmail.com>
2023-12-11 17:01:32 -0300

    first commit

---
+++ ImportacaoVitor/arquivo.js
@@ -0,0 +1,5 @@
+import{ somar } from "./functions";
+import{ alertTeste } from "./functions";
+
+somar (2,5);
+alertTeste();
---
+++ ImportacaoVitor/functions.js
@@ -0,0 +1,8 @@
+export function somar(a , b){
+    let add = a + b;
+    alert(add)
+}
+
+export function alertTeste(){
+    alert('Teste');
+}
